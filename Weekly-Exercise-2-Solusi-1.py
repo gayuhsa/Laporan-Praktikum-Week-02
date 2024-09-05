9bb4dbf7a20b5ef599b9bad2c7c6c775469d70d7
gayuhsa <senoaji13254@gmail.com>
2024-09-05 18:33:48 +0700

    Create Weekly-Exercise-2-Solusi-1.py

---
+++ Weekly-Exercise-2-Solusi-1.py
@@ -0,0 +1,17 @@
+nilai_shafira = 85 + 75
+print("Rata-rata nilai Shafira pada kedua mata kuliah: " + str(nilai_shafira / 2))
+
+nilai_hanif = 75 + 90
+nilai_andi = 70 + 75
+print("Jumlah nilai Hanif dan Andi untuk semua mata kuliah: " + str(nilai_hanif + nilai_andi))
+
+nilai_kalkulus_1 = 85 + 85 + 80 + 95
+nilai_metode_statistika = 85 + 85 + 75 + 80
+print("Rata-rata nilai Widya, Dhanar, Hikma dan Nedia pada mata kuliah Kalkulus 1: " + str(nilai_kalkulus_1 / 4))
+print("Rata-rata nilai Widya, Dhanar, Hikma dan Nedia pada mata kuliah Metode Statistika: " + str(nilai_metode_statistika / 4))
+
+total_nilai_kalkulus_1 = 85 + 80 + 75 + 95 + 85 + 75 + 70 + 85 + 80
+print("Rata-rata nilai mata kuliah Kalkulus 1 untuk semua mahasiswa: " + str(total_nilai_kalkulus_1 / 9))
+
+total_nilai_metode_statistika = 75 + 90 + 80 + 80 + 85 + 90 + 75 + 85 + 75
+print("Rata-rata nilai mata kuliah Metode Statistika untuk semua mahasiswa: " + str(total_nilai_metode_statistika / 9))
