c66894261d0bd59de1c56b46392988913b5e68b2
gayuhsa <senoaji13254@gmail.com>
2024-09-05 18:32:50 +0700

    Create Weekly-Exercise-1.py

---
+++ Weekly-Exercise-1.py
@@ -0,0 +1,13 @@
+panjang = 20.5
+lebar = 25
+tinggi = 10
+
+keliling = 2 * (panjang + lebar)
+luas = panjang * lebar
+volume = panjang * lebar * tinggi
+
+print("Panjang = " + str(panjang))
+print("Lebar = " + str(lebar))
+print("Tinggi = "  + str(tinggi))
+print("Keliling = " + str(keliling))
+print("Volume = " + str(volume))
